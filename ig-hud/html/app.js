c779a37b65007b79d9f5524cc823005d05094842
Ignelis91 <110096365+Ignelis91@users.noreply.github.com>
2022-08-30 17:14:09 +0300

    Add files via upload

---
+++ ig-hud/html/app.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', function() {
+})
+
+let moneyFormat = Intl.NumberFormat('es-ES')
+window.addEventListener('message', function(event) {
+    var v = event.data
+
+    switch(v.action) {
+        case 'updateTalking': 
+            $('.userid').text(v.pid)
+        break;
+
+        case 'UpdateData':
+            UpdateData(v.key, v.value, v.key2, v.value2)
+        break; 
+    }
+})
+
+function UpdateData(key, value, key2, value2) {
+    
+    if (key2 == 'grado') {
+        $(`.${key}`).text(value)
+        $(`.${key2}`).text(value2)
+    } else {
+        $(`.${key}`).text(moneyFormat.format(value))
+    }
+
+}
